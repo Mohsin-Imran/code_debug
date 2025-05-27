66b6ac483c4b6fed41e90a375e44521d2928977e
Mohsin-Imran <mohsin.centosquare@gmail.com>
2025-05-27 05:14:17 +0500

    first commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'AI Code Debug',
+  title: 'AI Code Debug Tool',
   description: 'Created with AI Code Debug',
   generator: 'AI Code Debug',
 }
